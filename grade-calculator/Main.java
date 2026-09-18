e3e7dd63322239ee278c3b66b6f3464e0c25fdc1
Ritchie Boy Romagos <ritchiebromagos7@gmail.com>
2026-09-18 21:20:56 +0800

    new update

--- grade-calculator/Main.java
+++ grade-calculator/Main.java
@@ -1,39 +1,33 @@
 
-
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=============== STUDENT GRADE CALCULATOR =============");
+        System.out.println("=============== ASSIGNMENT DEADLINE TRACKER =============");
 
-        System.out.println("Enter Student Name: ");
-        String name = sc.nextLine();
+        System.out.println("Assignment Name: ");
+        String assignment = sc.nextLine();
 
         System.out.println("Enter Subject: ");
         String subject = sc.nextLine();
 
-        System.out.println("Enter Quiz Score (out of 100): ");
-        double quiz = Double.parseDouble(sc.nextLine());
+        System.out.println("Enter Due Date: ");
+        String dueDate = sc.nextLine();
 
-        System.out.println("Enter Exam Score (out of 100): ");
-        double exam = Double.parseDouble(sc.nextLine());
+        System.out.println("Enter The Priority (High/Medium/Low)");
+        String level = sc.nextLine();
 
-        double finalGrade = (quiz * 0.4) + (exam * 0.6);
+        System.out.println("Enter Status (Pending/Completed)");
+        String status = sc.nextLine();
 
-        System.out.println("\n ============= GRADE SUMMARY ==============");
-        System.out.println("Student: "+name);
+        System.out.println("\n ============= ASSIGNMENT INFORMATION ==============");
+        System.out.println("Assignment: "+assignment);
         System.out.println("Subject: "+subject);
-        System.out.println("Quiz Score: "+quiz);
-        System.out.println("Exam Score: "+exam);
-        System.out.println("Final Grade: "+finalGrade);
-
-        if(finalGrade >= 75){
-            System.out.println("Status: PASSED");
-        } else {
-            System.out.println("Status: FAILED");
-        }
+        System.out.println("Due Date: "+dueDate);
+        System.out.println("Priority: "+level);
+        System.out.println("Status: "+status);
 
         sc.close();
     }
